05e53febe03555da07dc04e774860bb3c66b6fe1
s4elvale <s4elvale@uni-trier.de>
2022-12-01 18:24:39 +0100

    remove unnecessary JApplet extension

--- Pacman.java
+++ Pacman.java
@@ -2,13 +2,12 @@
 
 import javax.swing.*;
 import java.awt.event.*;
-import javax.swing.JApplet;
 import java.awt.*;
 import java.lang.*;
 
 /* This class contains the entire game... most of the game logic is in the Board class but this
    creates the gui and captures mouse and keyboard input, as well as controls the game states */
-public class Pacman extends JApplet implements MouseListener, KeyListener {
+public class Pacman implements MouseListener, KeyListener {
 
     /* These timers are used to kill title, game over, and victory screens after a set idle period (5 seconds)*/
     long titleTimer = -1;
